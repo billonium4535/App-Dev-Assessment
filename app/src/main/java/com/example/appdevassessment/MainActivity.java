9667b9464c2524184b333b547c68eb8b8f643df6
billonium <billy.sethgrimmett@gmail.com>
2022-12-08 23:31:05 +0000

    Created bridge report page

--- app/src/main/java/com/example/appdevassessment/MainActivity.java
+++ app/src/main/java/com/example/appdevassessment/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.appdevassessment;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button reportBridgeButton = (Button) findViewById(R.id.report_bridge_button);
+
+        reportBridgeButton.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, ReportBridgeActivity.class)));
     }
 }
---
+++ app/src/main/java/com/example/appdevassessment/ReportBridgeActivity.java
@@ -0,0 +1,13 @@
+package com.example.appdevassessment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReportBridgeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_bridge);
+    }
+}
